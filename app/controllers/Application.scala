9742461b4bc5fe1c07a08de7c3beeecf520e3bfb
ubourdon <bourdon.ugo@gmail.com>
2012-03-12 14:25:24 +0100

    add: link devoxx on index page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,7 +11,7 @@ object Application extends Controller {
     Ok(views.html.index())
   }
 
-  def getFlux = Action {
+  def flux = Action {
     Ok(new CalendarStream().fetch).as("text/calendar")
   }
 
