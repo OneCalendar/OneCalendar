bbcc9603f3f8ff82a103c49cfe88252bdfff8c03
ubourdon <bourdon.ugo@gmail.com>
2012-03-12 18:51:31 +0100

    improve: ICalBuilder

--- app/service/ICalBuilder.scala
+++ app/service/ICalBuilder.scala
@@ -5,25 +5,22 @@ import net.fortuna.ical4j.model._
 import net.fortuna.ical4j.model.component._
 import property._
 import net.fortuna.ical4j.data.CalendarOutputter
-import java.io.StringWriter
+import java.io.{Writer, StringWriter}
 
 class ICalBuilder {
-    
     val ID: String = "-//OneCalendarToMeetThemAll//FR"
 
     def buildCalendar( events: List[Event] ): String = {
         val componentList: ComponentList = new ComponentList()
+
         events.map(
             event =>
                 componentList.add( buildVEvent(event) )
         )
-        val writer = new StringWriter()
-        val calendar: Calendar = new Calendar(componentList)
-        calendar.getProperties().add(Version.VERSION_2_0);
-        calendar.getProperties().add(new ProdId(ID));
-        calendar.getProperties().add(CalScale.GREGORIAN);
-        new CalendarOutputter().output(calendar, writer)
-        writer.toString
+
+        val calendar: Calendar = buildCalendar( componentList )
+
+        serializeCalendar( calendar )
     }
 
     private def buildVEvent( event: Event ): VEvent = {
@@ -39,5 +36,19 @@ class ICalBuilder {
         vevent.getProperties.add( new Location( event.location ) )
 
         vevent
+    }
+
+    private def buildCalendar( componentList: ComponentList ): Calendar = {
+        val calendar: Calendar = new Calendar( componentList )
+        calendar.getProperties.add( Version.VERSION_2_0 );
+        calendar.getProperties.add( new ProdId( ID ) );
+        calendar.getProperties.add( CalScale.GREGORIAN );
+        calendar
+    }
+
+    private def serializeCalendar(calendar: Calendar): String = {
+        val writer: Writer = new StringWriter()
+        new CalendarOutputter().output( calendar, writer )
+        writer.toString
     }
 }
