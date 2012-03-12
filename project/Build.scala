979ae564b1bf7fe7219dbe8f509a2f9827aa8734
ubourdon <bourdon.ugo@gmail.com>
2012-03-12 10:56:44 +0100

    add: jongo & mongo dependency

--- project/Build.scala
+++ project/Build.scala
@@ -4,16 +4,20 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "OneCalendar"
-    val appVersion      = "1.0-SNAPSHOT"
+    val appName = "OneCalendar"
+    val appVersion = "1.0-SNAPSHOT"
+
+    resolvers += "oss-sonatype" at "http://oss.sonatype.org/content/groups/public"
 
     val appDependencies = Seq(
-      "org.scalatest" %% "scalatest" % "1.7.1" % "test",
-      "org.joda" % "joda-convert" % "1.2"
+        "org.scalatest" %% "scalatest" % "1.7.1" % "test",
+        "org.joda" % "joda-convert" % "1.2",
+        "org.jongo" % "jongo" % "0.1-SNAPSHOT" changing(),
+        "org.mongodb" % "mongo-java-driver" % "2.6.5"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+        // Add your own project settings here
     )
 
 }
