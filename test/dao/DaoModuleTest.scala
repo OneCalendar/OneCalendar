e180fdf19eed4943fdc917fb306685e7f8d059e8
Amira LAKHAL <amira.lakhal@gmail.com>
2012-03-13 13:45:31 +0100

    add: ability to add and read event in mongodb

--- test/dao/DaoModuleTest.scala
+++ test/dao/DaoModuleTest.scala
@@ -1,10 +1,10 @@
 package dao
 
-import com.mongodb.{DB, Mongo}
-import org.jongo.MongoCollection
 import models.Event
 import org.joda.time.DateTime
 import collection.immutable.List
+import com.mongodb._
+import org.scalatest.{BeforeAndAfter}
 
 
 /**
@@ -12,28 +12,38 @@ import collection.immutable.List
  * Date: 09/03/12
  */
 
-class DaoModuleTest extends AbstractDaoTest {
+class DaoModuleTest extends AbstractDaoTest with BeforeAndAfter {
 
-  test("connecting to mongodb test") {
-     try{
-        val db: DB = DaoModule.getDatabase("test")
-        db.getName should be ("test")
-     }   finally {
-        //DaoModule.mongo.close()
-     }
-  }
-
-  test("getting events collection") {
-     val eventsCollection: MongoCollection = DaoModule.getEventsCollection("test")
-     eventsCollection.getName should be ("events")
+    test("connecting to mongodb test") {
+        try {
+            val db: DB = DaoModule.getDatabase("test")
+            db.getName should be("test")
+        } finally {
+        }
     }
 
-  test("saving a new event") {
-    val event : Event = new Event("1","BOF",new DateTime(2012,04,19,0,0,0,0),new DateTime(2012,04,19,0,0,0,0),"","", List("java", "devoxx"))
-    DaoModule.saveEvent("test",event)
+    test("getting events collection") {
+        val eventsCollection: DBCollection = DaoModule.getEventsCollection("test")
+        eventsCollection.getName should be("events")
+    }
 
-    val savedEvent  = DaoModule.getEventsCollection("test").count("{}")
-    savedEvent should be (1)
-  }
 
+
+    test("saving a new event") {
+        val event: Event = new Event("1", "BOF", new DateTime(2012, 04, 19, 0, 0, 0, 0), new DateTime(2012, 04, 19, 0, 0, 0, 0), "", "", List("java", "devoxx"))
+        DaoModule.saveEvent("test", event)
+
+        val eventsCollection: DBCollection = DaoModule.getEventsCollection("test")
+        val savedEvent = eventsCollection.count()
+        savedEvent should be(1)
+
+        val one: DBObject = eventsCollection.findOne()
+
+        DaoModule.fromDbObject2Event(one) should be(event)
+
+    }
+
+    def after() {
+        DaoModule.getDatabase("test").requestDone()
+    }
 }
