7905a5213820b81698ed48752011ab29ab511788
ubourdon <bourdon.ugo@gmail.com>
2012-03-13 19:49:45 +0100

    add: hacking loader devoxx calendar in mongo OneCalendar database

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,10 @@
 package controllers
 
 import play.api.mvc._
-import service.{CalendarStream, ICalBuilder}
 import models.Event
 import dao.configuration.injection.MongoConfiguration
 import dao.EventDao
+import service.{LoadICalStream, CalendarStream, ICalBuilder}
 
 object Application extends Controller {
 
@@ -23,6 +23,13 @@ object Application extends Controller {
     def findByTags( keyWords: String ) = Action {
         val tags: List[String] = keyWords.split(" ").toList
         renderEvents( EventDao.findByTag( tags ) )
+    }
+    
+    def loadDevoxxCalendar = Action {
+        val url : String = "https://www.google.com/calendar/ical/u74tb1k9n53bnc5qsg3694p2l4%40group.calendar.google.com/private-4b4d566cd18fd63d76c6cc6ea84086cf/basic.ics"
+        val iCalService : LoadICalStream = new LoadICalStream()
+        iCalService.parseLoad( url )
+        Ok("base " + mongoConfigProd.dbName + " loaded with devoxx Calendar")
     }
 
     private def renderEvents( events: List[ Event ] ) = {
--- test/service/LoadICalStreamTest.scala
+++ test/service/LoadICalStreamTest.scala
@@ -27,7 +27,7 @@ class LoadICalStreamTest extends FunSuite with ShouldMatchers with BeforeAndAfte
         db.requestDone
     }
 
-    test("should parse iCal stream"){
+    test("should parse iCal stream") {
         val url : String = "https://www.google.com/calendar/ical/u74tb1k9n53bnc5qsg3694p2l4%40group.calendar.google.com/private-4b4d566cd18fd63d76c6cc6ea84086cf/basic.ics"
         val iCalService : LoadICalStream = new LoadICalStream()
         iCalService.parseLoad( url )
