c3a3d4515bccadc4ecf8f1810d4a1ce08eb02c7e
ubourdon <bourdon.ugo@gmail.com>
2012-04-12 23:51:35 +0200

    improve: redesign index page for devoxx conference

--- public/javascripts/iconsHelp.js
+++ public/javascripts/iconsHelp.js
@@ -9,7 +9,21 @@ $(function() {
             });
     }
 
+    function handleHelpDevoxx(clzz, message) {
+        $(clzz)
+            .mouseover(function() {
+                $(this).siblings(".helpDevoxx").text(message);
+            })
+            .mouseout(function() {
+                $(this).siblings(".helpDevoxx").text("");
+            });
+    }
+
     handleHelp(".ical", "Utilisez l'url brut pour windows avec Outlook ainsi que pour les autres plateforme");
     handleHelp(".gcal", "Pour ajouter l'agenda à ceux que vous avez déjà sur google agenda ou si vous êtes sur Android");
     handleHelp(".webcal", "Si votre navigateur supporte les URLs webcal:// comme sur MacOS ou iOS");
+
+    handleHelpDevoxx(".ical", "flux Devoxx brut, marche pour windows Outlook");
+    handleHelpDevoxx(".gcal", "flux Devoxx pour google Agenda ou Android");
+    handleHelpDevoxx(".webcal", "flux Devoxx pour ICal Apple (ios) format webcal");
 });
