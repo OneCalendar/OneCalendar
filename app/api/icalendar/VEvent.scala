b9b36f77682f0eb8bbfd455c1804cf28abd33a21
ubourdon <bourdon.ugo@gmail.com>
2012-11-12 17:21:08 +0100

    add: equals method for VEvent class

--- app/api/icalendar/VEvent.scala
+++ app/api/icalendar/VEvent.scala
@@ -4,23 +4,32 @@ import org.joda.time.DateTime
 import net.fortuna.ical4j.model.property.DateProperty
 import net.fortuna.ical4j.model.Property
 
-class VEvent( vevent: net.fortuna.ical4j.model.component.VEvent ) {
+class VEvent(vevent: net.fortuna.ical4j.model.component.VEvent) {
     require(vevent != null, "requirement failed : net.fortuna.ical4j.model.component.VEvent should not be null")
 
-    def uid: Option[String] = option(vevent.getUid, toValue)
-    def summary: Option[String] = option(vevent.getSummary, toValue)
+    def uid: Option[ String ] = option(vevent.getUid, toValue)
 
-    def startDate: Option[DateTime] = option(vevent.getStartDate, toDate)
-    def endDate: Option[DateTime] = option(vevent.getEndDate, toDate)
+    def summary: Option[ String ] = option(vevent.getSummary, toValue)
 
-    def location: Option[String] = option(vevent.getLocation, toValue)
-    def url: Option[String] = option(vevent.getUrl, toValue)
-    def description: Option[String] = option(vevent.getDescription, toValue)
+    def startDate: Option[ DateTime ] = option(vevent.getStartDate, toDate)
+
+    def endDate: Option[ DateTime ] = option(vevent.getEndDate, toDate)
+
+    def location: Option[ String ] = option(vevent.getLocation, toValue)
+
+    def url: Option[ String ] = option(vevent.getUrl, toValue)
+
+    def description: Option[ String ] = option(vevent.getDescription, toValue)
+
+    override def equals(vevent: Any): Boolean = vevent match {
+        case that: VEvent => if ( this.uid == None || that.uid == None ) false else this.uid == that.uid
+        case _ => false
+    }
 
     // TODO méfions nous des getDate de fortuna
     val toDate = (p: DateProperty) => new DateTime(p.getDate)
     val toValue = (p: Property) => p.getValue
-    
-    private def option[T,U <: Property](property: U, f: (U) => T): Option[T] =
-        if (property != null) Option(f(property)) else None
+
+    private def option[T, U <: Property](property: U, f: (U) => T): Option[T] =
+        if ( property != null ) Option(f(property)) else None
 }
--- test/api/icalendar/VEventTest.scala
+++ test/api/icalendar/VEventTest.scala
@@ -20,7 +20,7 @@ class VEventTest extends FunSuite with ShouldMatchers {
     }
 
     test("should have VEvent case class encapsulate VEVENT specification in ICalendar") {
-        val vEvent = new VEvent(getVEvent)
+        val vEvent = new VEvent(getVEvent())
         
         vEvent.uid should be (Option("123"))
         vEvent.summary should be (Option("title"))
@@ -39,10 +39,16 @@ class VEventTest extends FunSuite with ShouldMatchers {
         exception.getMessage should include ("VEvent should not be null")
     }
 
-    private def getVEvent: net.fortuna.ical4j.model.component.VEvent = {
+    test("define equals method") {
+        new VEvent(getVEvent()) should equal (new VEvent(getVEvent()))
+        new VEvent(getVEvent(uid = false)) should not equal(new VEvent(getVEvent(uid = false)))
+        new VEvent(getVEvent()) should not equal ("")
+    }
+    
+    private def getVEvent(uid: Boolean = true): net.fortuna.ical4j.model.component.VEvent = {
         val vevent = new net.fortuna.ical4j.model.component.VEvent()
 
-        vevent.getProperties.add(new Uid("123"))
+        if(uid) vevent.getProperties.add(new Uid("123"))
 
         vevent.getProperties.add( new DtStart( new net.fortuna.ical4j.model.DateTime(123456L) ) )
         vevent.getProperties.add( new DtEnd( new net.fortuna.ical4j.model.DateTime(456457L) ) )
