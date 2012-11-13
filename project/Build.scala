963e859be74a5d28c3e5850150bd73ca814d8ba0
BabylonZeus <patmanoukian@gmail.com>
2012-11-13 16:02:24 +0100

    add: mockito dependency

--- project/Build.scala
+++ project/Build.scala
@@ -9,11 +9,13 @@ object ApplicationBuild extends Build {
     val appVersion = "1.0-SNAPSHOT"
 
     resolvers += "oss-sonatype" at "http://oss.sonatype.org/content/groups/public"
+    resolvers += "oss-sonatype-releases" at "http://oss.sonatype.org/content/repositories/releases/"
 
     val appDependencies = Seq(
         "org.scalatest" %% "scalatest" % "1.8" % "test",
         "org.mongodb" % "mongo-java-driver" % "2.7.3",
         "com.codahale" % "jerkson_2.9.1" % "0.5.0",
+        "org.mockito" % "mockito-all" % "1.9.0" % "test",
         "org.mnode.ical4j" % "ical4j" % "1.0.3" excludeAll(
             ExclusionRule(organization = "org.slf4j"),
             ExclusionRule(organization = "commons-logging")
