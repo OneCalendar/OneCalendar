cd0b39f2c27288d68c19dc7b96e9bd86b77ed2e1
BabylonZeus <patmanoukian@gmail.com>
2012-11-13 16:02:52 +0100

    add: test EventDao findPreviewByTag

--- app/dao/EventDaoTrait.scala
+++ app/dao/EventDaoTrait.scala
@@ -4,6 +4,7 @@ import configuration.injection.MongoConfiguration
 import models.SearchPreview
 import models.Event
 
+//ToDo: this trait was added only to allow mocks into tests
 trait EventDaoTrait {
   def deleteAll()(implicit dbConfig: MongoConfiguration)
 
