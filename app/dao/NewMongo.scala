c03600ae88e0144cdcefd02842eac4cdd4e755ec
ubourdon <bourdon.ugo@gmail.com>
2012-11-15 11:18:45 +0100

    improve: version of casbah mongodb driver from 2.1.4-1 to 2.4.1 with good repo

--- app/dao/NewMongo.scala
+++ app/dao/NewMongo.scala
@@ -4,7 +4,7 @@ import com.mongodb.casbah.Imports._
 import models.Event
 import org.joda.time.DateTime
 
-trait MongoDbModel[ T ] {
+trait MongoDbModel[T] {
     def collectionName: String
 
     def write(t: T): DBObject
@@ -13,42 +13,42 @@ trait MongoDbModel[ T ] {
 }
 
 trait MongoOperations {
-    def save[ T ](t: T)(implicit tc: MongoDbModel[ T ], coll: String => MongoCollection): Unit =
+    def save[T](t: T)(implicit tc: MongoDbModel[T], coll: String => MongoCollection): Unit =
         coll(tc.collectionName).insert(tc.write(t))
 
-    def findOne[ T ](query: DBObject)(implicit tc: MongoDbModel[ T ], coll: String => MongoCollection):
-    Option[ T ] = coll(tc.collectionName).findOne(query).map {
+    def findOne[T](query: DBObject)(implicit tc: MongoDbModel[T], coll: String => MongoCollection):
+    Option[T] = coll(tc.collectionName).findOne(query).map {
         tc.read _
     }
 
-    def findById[ T ](id: ObjectId)(implicit tc: MongoDbModel[ T ], coll: String => MongoCollection):
-    Option[ T ] = coll(tc.collectionName).findOneByID(id).map {
+    def findById[T](id: ObjectId)(implicit tc: MongoDbModel[T], coll: String => MongoCollection):
+    Option[T] = coll(tc.collectionName).findOneByID(id).map {
         tc.read _
     }
 
-    def find[ T ](query: DBObject)(implicit tc: MongoDbModel[ T ], coll: String => MongoCollection):
-    List[ T ] = coll(tc.collectionName).find(query).map {
+    def find[T](query: DBObject)(implicit tc: MongoDbModel[T], coll: String => MongoCollection):
+    List[T] = coll(tc.collectionName).find(query).map {
         tc.read _
     }.toList
 
-    def find[ T ](query: DBObject, sort: DBObject, limit: Int)(implicit tc: MongoDbModel[ T ], coll: String => MongoCollection):
-    List[ T ] = coll(tc.collectionName).find(query).sort(sort).limit(limit).map {
+    def find[T](query: DBObject, sort: DBObject, limit: Int)(implicit tc: MongoDbModel[T], coll: String => MongoCollection):
+    List[T] = coll(tc.collectionName).find(query).sort(sort).limit(limit).map {
         tc.read _
     }.toList
 
-    def delete[ T ](query: DBObject)(implicit tc: MongoDbModel[ T ], coll: String => MongoCollection):
+    def delete[T](query: DBObject)(implicit tc: MongoDbModel[T], coll: String => MongoCollection):
     Unit = coll(tc.collectionName).remove(query)
 
-    def count[ T ](implicit tc: MongoDbModel[ T ], coll: String => MongoCollection): Long =
+    def count[T](implicit tc: MongoDbModel[T], coll: String => MongoCollection): Long =
         coll(tc.collectionName).count
 
-    def count[ T ](query: DBObject)(implicit tc: MongoDbModel[ T ], coll: String => MongoCollection): Long =
+    def count[T](query: DBObject)(implicit tc: MongoDbModel[T], coll: String => MongoCollection): Long =
         coll(tc.collectionName).count(query)
 }
 
 object EventMap {
 
-    implicit object EventMongoModel extends MongoDbModel[ Event ] {
+    implicit object EventMongoModel extends MongoDbModel[Event] {
         def collectionName: String = "events"
 
         def write(event: Event): DBObject =
@@ -66,15 +66,16 @@ object EventMap {
 
         def read(dbo: DBObject): Event =
             Event(
-                begin = new DateTime(dbo.as[ Long ]("begin")),
-                description = dbo.as[ String ]("description"),
-                end = new DateTime(dbo.as[ Long ]("end")),
-                location = dbo.as[ String ]("location"),
-                originalStream = dbo.as[ String ]("originalStream"),
-                tags = dbo.as[ BasicDBList ]("tags").toList.asInstanceOf[ List[ String ] ],
-                title = dbo.as[ String ]("title"),
-                uid = dbo.as[ String ]("uid"),
-                url = dbo.as[ String ]("url")
+                begin = new DateTime(dbo.as[Long]("begin")),
+                description = dbo.as[String]("description"),
+                end = new DateTime(dbo.as[Long]("end")),
+                location = dbo.as[String]("location"),
+                originalStream = dbo.as[String]("originalStream"),
+                tags = dbo.as[MongoDBList]("tags").toList.asInstanceOf[List[String]],
+                title = dbo.as[String]("title"),
+                uid = dbo.as[String]("uid"),
+                url = dbo.as[String]("url")
             )
     }
+
 }
--- project/Build.scala
+++ project/Build.scala
@@ -12,24 +12,25 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
         "org.scalatest" %% "scalatest" % "1.8" % "test",
         "com.codahale" %% "jerkson" % "0.5.0",
-        "com.mongodb.casbah" %% "casbah" % "2.1.5-1",
+        "org.mongodb" %% "casbah" % "2.4.1",
         "org.mockito" % "mockito-all" % "1.9.0" % "test",
         "org.mnode.ical4j" % "ical4j" % "1.0.3" excludeAll(
             ExclusionRule(organization = "org.slf4j"),
             ExclusionRule(organization = "commons-logging")
-        )
+            )
     )
 
-  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
 
         resolvers += "oss-sonatype" at "http://oss.sonatype.org/content/groups/public",
         resolvers += "oss-sonatype-releases" at "http://oss.sonatype.org/content/repositories/releases/",
-        resolvers += "scala-tools"  at "http://oss.sonatype.org/content/groups/scala-tools/",
+        /*resolvers += "scala-tools"  at "http://oss.sonatype.org/content/groups/scala-tools/",*/
 
-        testOptions in Test := Nil,  //to run scalatest in play2 console arghhhh!!!
+        testOptions in Test := Nil, //to run scalatest in play2 console arghhhh!!!
 
         // available test resources in play2 classpath
-        unmanagedClasspath in Test <+= (baseDirectory) map { bd => Attributed.blank(bd / "test") }
+        unmanagedClasspath in Test <+= ( baseDirectory ) map {
+            bd => Attributed.blank(bd / "test")
+        }
     )
-
 }
