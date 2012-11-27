48ee42df7d91f76f186718ec7881c63cf45b2b49
ubourdon <bourdon.ugo@gmail.com>
2012-11-27 18:23:50 +0100

    fix: exception when search event without uid

--- app/dao/MongoOperations.scala
+++ app/dao/MongoOperations.scala
@@ -12,35 +12,23 @@ trait MongoDbModel[T] {
 }
 
 trait MongoOperations {
-    def save[T](t: T)(implicit tc: MongoDbModel[T], coll: MongoCollectionName => MongoCollection): Unit = {
+    def save[T](t: T)(implicit tc: MongoDbModel[T], coll: MongoCollectionName => MongoCollection): Unit =
         coll(tc.collectionName).insert(tc.write(t))
-    }
 
-    def findOne[T](query: DBObject)(implicit tc: MongoDbModel[T], coll: MongoCollectionName => MongoCollection): Option[T] = {
-        coll(tc.collectionName).findOne(query).map {
-            tc.read _
-        }
-    }
+    def findOne[T](query: DBObject)(implicit tc: MongoDbModel[T], coll: MongoCollectionName => MongoCollection): Option[T] =
+        coll(tc.collectionName).findOne(query).map( dbo => tc.read(dbo) )
 
-    def findById[T](id: ObjectId)(implicit tc: MongoDbModel[T], coll: MongoCollectionName => MongoCollection): Option[T] = {
-        coll(tc.collectionName).findOneByID(id).map {
-            tc.read _
-        }
-    }
+    def findById[T](id: ObjectId)(implicit tc: MongoDbModel[T], coll: MongoCollectionName => MongoCollection): Option[T] =
+        coll(tc.collectionName).findOneByID(id).map( dbo => tc.read(dbo) )
 
-    def find[T](query: DBObject)(implicit tc: MongoDbModel[T], coll: MongoCollectionName => MongoCollection): List[T] = {
-        coll(tc.collectionName).find(query).map(tc.read _).toList
-    }
+    def find[T](query: DBObject)(implicit tc: MongoDbModel[T], coll: MongoCollectionName => MongoCollection): List[T] =
+        coll(tc.collectionName).find(query).map( dbo => tc.read(dbo) ).toList
 
-    def find[T](query: DBObject, sort: DBObject, limit: Int)(implicit tc: MongoDbModel[T], coll: MongoCollectionName => MongoCollection): List[T] = {
-        coll(tc.collectionName).find(query).sort(sort).limit(limit).map {
-            tc.read _
-        }.toList
-    }
+    def find[T](query: DBObject, sort: DBObject, limit: Int)(implicit tc: MongoDbModel[T], coll: MongoCollectionName => MongoCollection): List[T] =
+        coll(tc.collectionName).find(query).sort(sort).limit(limit).map( dbo => tc.read(dbo) ).toList
 
-    def delete[T](query: DBObject)(implicit tc: MongoDbModel[T], coll: MongoCollectionName => MongoCollection): Unit = {
+    def delete[T](query: DBObject)(implicit tc: MongoDbModel[T], coll: MongoCollectionName => MongoCollection): Unit =
         coll(tc.collectionName).remove(query)
-    }
 
     def count[T](implicit tc: MongoDbModel[T], coll: MongoCollectionName => MongoCollection): Long =
         coll(tc.collectionName).count
