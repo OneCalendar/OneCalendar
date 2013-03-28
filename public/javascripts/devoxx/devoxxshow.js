2c40cdf7e28a5781e7fdcb34f11d31183a011e1e
Nicolas Bétheuil <nicolas.betheuil@valtech.fr>
2013-03-28 16:04:37 +0100

    display: relative dates for events

--- public/javascripts/devoxx/devoxxshow.js
+++ public/javascripts/devoxx/devoxxshow.js
@@ -5,15 +5,69 @@ $(document).ready(function hello(){
 
     var reload;
 
+    moment.lang('fr', {
+        months : "janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre".split("_"),
+        monthsShort : "janv._févr._mars_avr._mai_juin_juil._août_sept._oct._nov._déc.".split("_"),
+        weekdays : "dimanche_lundi_mardi_mercredi_jeudi_vendredi_samedi".split("_"),
+        weekdaysShort : "dim._lun._mar._mer._jeu._ven._sam.".split("_"),
+        weekdaysMin : "Di_Lu_Ma_Me_Je_Ve_Sa".split("_"),
+        longDateFormat : {
+            LT : "HH:mm",
+            L : "DD/MM/YYYY",
+            LL : "D MMMM YYYY",
+            LLL : "D MMMM YYYY LT",
+            LLLL : "dddd D MMMM YYYY LT"
+        },
+        calendar : {
+            sameDay: "[Aujourd'hui à] LT",
+            nextDay: '[Demain à] LT',
+            nextWeek: 'dddd [à] LT',
+            lastDay: '[Hier à] LT',
+            lastWeek: 'dddd [dernier à] LT',
+            sameElse: 'L'
+        },
+        relativeTime : {
+            future : "dans %s",
+            past : "il y a %s",
+            s : "quelques secondes",
+            m : "une minute",
+            mm : "%d minutes",
+            h : "une heure",
+            hh : "%d heures",
+            d : "un jour",
+            dd : "%d jours",
+            M : "un mois",
+            MM : "%d mois",
+            y : "une année",
+            yy : "%d années"
+        },
+        ordinal : function (number) {
+            return number + (number === 1 ? 'er' : 'ème');
+        },
+        week : {
+            dow : 1, // Monday is the first day of the week.
+            doy : 4  // The week that contains Jan 4th is the first week of the year.
+        }
+    });
+
     function templateThisEvent(event) {
         event = event || {
             uid:"",tags:[],location:"",description:"",begin:0,end:0,originalStream:"",title:""
         };
 
         function toDateDisplayed(event) {
+            var now = moment(new Date());
             var begin = moment(event.begin);
             var end = moment(event.end);
-            return begin.format("DD/MM/YYYY  HH:mm") + " - " + end.format("HH:mm");
+            if (begin.isAfter(now)) {
+                return " Début dans %s".replace("%s",begin.fromNow(true));
+            } else {
+                if (end.isAfter(now)) {
+                    return "Fin %s".replace("%s",end.fromNow());
+                } else {
+                    return "Fini %s".replace("%s",end.fromNow());
+                }
+            }
         }
 
         $("#rows").append(TEMPLATE_EVENT
