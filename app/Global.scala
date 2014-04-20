c25e942a2c54ade97b0d687026c5d6eeda616af5
ubourdon <bourdon.ugo@gmail.com>
2014-04-20 11:35:16 +0200

    remove: devoxx france schedule

--- app/Global.scala
+++ app/Global.scala
@@ -48,7 +48,7 @@ object Global extends GlobalSettings with MongoDBProdContext {
             Logger.trace("reload")
         }
 
-        Akka.system.scheduler.schedule(30 seconds, 2 minutes) {
+        /*Akka.system.scheduler.schedule(30 seconds, 2 minutes) {
             Logger.info("Chargement devoxx")
             implicit val now = () => DateTime.now.getMillis
 
@@ -57,7 +57,7 @@ object Global extends GlobalSettings with MongoDBProdContext {
             } catch {
                 case e: Exception => Logger.error("something wrong with load devoxx : ", e)
             }
-        }
+        }*/
 
         Akka.system.scheduler.schedule(5 seconds, 1 day) { LoadEventbrite.parseLoad("scala") }
     }
